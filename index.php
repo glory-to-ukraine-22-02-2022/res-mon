50f25a3a77140e88a4526970219d362b145cfaed
glory-to-ukraine-22-02-2022 <101142077+glory-to-ukraine-22-02-2022@users.noreply.github.com>
2022-03-09 18:07:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1 class="text-center">Моніторинг ресурсів</h1>
+    <div style="max-width:50%; margin: 50px auto;">
+<textarea name="input" id="txt-input" class="form-control" cols="10" rows="5">
+https://www.urban-vpn.com/
+https://passport.yandex.ru
+https://online.vtb.ru/login
+https://www.sberbank.ru/
+http://kremlin.ru/events/president/news
+https://cabinet.mil.ru/
+nalog.gov.ru
+http://www.government.ru/
+http://www.kremlin.ru/
+http://kremlin.ru/structure/administration
+http://www.scrf.gov.ru/
+http://www.kremlin.ru/structure/state-council
+http://sovetnational.ru/
+http://president-sovet.ru/
+http://www.council.gov.ru/
+http://council.gov.ru/structure/chamber/
+http://legislation.council.gov.ru/
+http://region.council.gov.ru/
+http://international.council.gov.ru/
+http://budget.council.gov.ru/
+http://economy.council.gov.ru/
+http://agrarian.council.gov.ru/
+http://social.council.gov.ru/
+http://science.council.gov.ru/
+http://reglament.council.gov.ru/
+http://www.duma.gov.ru/
+http://duma.gov.ru/duma/council/
+http://www.scrf.gov.ru/
+ns1.yandex.ru
+213.180.193.1
+ns2.yandex.ru
+93.158.134.1
+https://www.1tv.ru
+https://more.tv/online/russia1_hd
+https://smotrim.ru/brand/21385
+https://smotrim.ru/brand/60851
+https://www.vokrug.tv/product/show/poedinok/
+https://vesti7.ru
+https://kino.mail.ru/tvshow_851444_vremya_pokazhet/
+https://www.1tv.ru/shows/vystupleniya-prezidenta-rossii
+https://tvzvezda.ru
+https://ren.tv
+life.ru
+rbc.ru
+https://www.liveinternet.ru/
+http://cbr.ru
+https://bg14.org/
+https://mosmetro.ru/
+https://rkn.gov.ru/
+nornickel.com
+lukoil.ru
+https://www.evraz.com/ru/
+https://bmpvsu.ru/
+surgutneftegas.ru
+nlmk.com
+sibur.ru
+severstal.com
+https://nangs.org/
+https://rmk-group.ru/ru/
+https://www.eurosib.ru/
+https://omk.ru/
+www.gazprombank.ru
+https://www.mos.ru/uslugi/
+pfr.gov.ru
+customs.gov.ru/
+gosuslugi.ru
+https://www.polymetalinternational.com/ru/
+https://www.uralkali.com/ru/
+https://www.metalloinvest.com/
+tatneft.ru
+https://russian.rt.com/
+https://ukraina.ru/
+http://life.ru/
+http://www.rbc.ru/
+http://www.vedomosti.ru/
+https://www.gazeta.ru/
+http://lenta.ru/
+http://www.interfax.ru/
+http://www.kommersant.ru/
+http://www.vesti.ru/
+https://tvrain.ru/
+https://ria.ru/
+https://rg.ru/
+http://izvestia.ru/
+https://tass.ru/
+https://www.rbc.ru/
+https://www.ntv.ru/
+http://defence.council.gov.ru/
+https://360tv.ru/
+https://www.kp.ru/
+https://www.fontanka.ru/
+https://www.mk.ru/
+https://news.ru/
+https://www.investing.com/
+https://sputniknews.com
+https://eadaily.com/
+https://vesti.ua/
+mrespublika.ru
+fsdnr.com
+voroshilovka.ru
+gisnpa-dnr.ru
+minfindnr.ru
+www.vodadonbassa.ru
+rst-dnr.ru
+khartsyzsk.ugletele.com
+fpdnr.ru
+goskomzemdnr.ru
+gkecopoldnr.ru
+dnr-live.ru
+wlad.eu
+mintek-dnr.ru
+dokuchaevskga.ucoz.org
+novoaz-adm.3dn.ru
+starobeshevo.usite.pro
+www.shahtersk.org
+minstroy-dnr.ru
+crb-dnr.ru
+gostrud-dnr.ru
+gorod-donetsk.com
+donmintrans.ru
+admin-gorlovka.ru
+doncsm.ru
+donmarkets.ru
+pfdnr.ru
+mer.govdnr.ru
+izpp.govdnr.ru
+makeyevka.ru
+mtspdnr.ru
+mdsdnr.ru
+zhdanovka.jimdo.com
+xn----etbafsqscydaxc.xn--p1acf
+xn--80aafc4b.xn--p1acf
+xn--80abi0beva.xn--p1acf
+xn----7sbn0bpov.xn--p1acf
+gum-centr.su
+xn----jtbhhlwgfdgt4h.xn--p1acf
+odsd.ru
+mzdnr.ru
+mincult.govdnr.ru
+mcxdnr.ru
+xn--80aaaco8cecf1aae8d.xn--p1acf
+postdonbass.com
+gazprom.ru
+минспорт.рус
+xn--b1akbpgy3fwa.xn--p1acf
+armiyadnr.su
+druss.ru
+novorossia.su
+supcourt-dpr.su
+gum-center.su
+mid-dnr.su
+gpdnr.su
+dnr24.com
+dnr24.su
+torez24.ru
+gkgtn.ru
+snezh-dnr.wmsite.ru
+guc-dnr.ru
+parlament-novorossii.ru
+mgb-dnr.ru
+3mob.ru
+dnrexport.ru
+drkm-dnr.ru
+vts1.ru
+zdravdnr.ru
+xn----7sbyb2abcjdpdb2a.xn--p1acf
+xn--b1aea3ais.xn--p1acf
+xn--c1aeaazcckr9ah.xn--p1acf
+smdnr.ru
+gpdnr.ru
+minsvyazdnr.ru
+mvddnr.ru
+mpt-dnr.ru
+dnmchs.ru
+glavstat.govdnr.ru
+mail1.mil.ru
+rcz-dnr.ru
+phoenix-dnr.ru
+guprec.ru
+amvrosievka-gorsovet.umi.ru
+minsvyazdnr.ru
+mail2.mil.ru
+tk-union.tv
+russian-center.ru
+dnr-board.com
+forum-novorossia.ru
+https://shop-rt.com
+http://leakmind.one/
+https://nothub.net/
+https://ria.ru/20220227/ukraina-1775323215.html
+https://www.km20.ru/
+http://twinsgarden.ru
+https://she.wrf.su
+https://belugamoscow.ru/
+https://biologie.ru/
+https://delivery.sakhalin-moscow.ru/
+https://delivery-club.ru
+https://5ka.ru
+https://magnit.ru
+https://zakupki.gov.ru
+https://www.subscribestar.com/iskra
+https://Sberbank-ast.ru
+https://Etp.roseltorg.ru
+https://Zakazrf.ru
+https://Etp-ets.ru
+https://Rts-tender.ru
+https://Gz.lot-online.ru
+https://Tektorg.ru
+https://Etpgpb.ru
+https://www.gias.by
+https://butb.by
+https://goszakupki.by
+http://chechnya.gov.ru/
+https://smotrim.ru/
+https://xn--c1aea8akcddj.xn--p1ai/
+https://mail.rkn.gov.ru
+https://cloud.rkn.gov.ru
+https://mvd.gov.ru
+https://pwd.wto.economy.gov.ru/
+https://stroi.gov.ru/
+https://proverki.gov.ru/
+https://president.gov.by/
+https://www.mil.by/ru/
+https://www.court.gov.by/
+https://minsk.gov.by/
+https://customs.gov.by/
+https://gomel.gov.by/
+https://airport.by/
+http://maz.by/
+https://www.belarus.by/
+https://www.belta.by/
+</textarea>
+
+        <div class="mt-3">
+            <label>
+                <div class="d-inline-flex">
+                    <span class="text-nowrap">Оновлювати кожні:&nbsp;</span>
+                    <span>
+                        <input class="js-interval form-control" type="number" value="10">
+                        <span class="alert alert-info small">"0" - не оновлювати</span>
+                    </span>
+                    <span>&nbsp;хвилин</span>
+                </div>
+            </label>
+        </div>
+        <div class="mt-3">
+            <button class="js-check btn btn-primary mt-3">Перевірити</button>
+        </div>
+    </div>
+
+    <table class="table table-striped table-sm mt-5" style="max-width:50%; margin: 50px auto;">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Ресурс</th>
+                <th scope="col">Статус</th>
+                <th scope="col">Сфера</th>
+            </tr>
+        </thead>
+        <tbody class="js-table-data">
+        </tbody>
+    </table>
+<script>
+    var $tableData = $('.js-table-data');
+    function requestRes(r) {
+        $.ajax({
+            url: '/status.php',
+            data: { val: r },
+            success(data) {
+                $(`[data-href="${r}"]`)
+                    .empty()
+                    .append(`<b class="badge badge-${data ? 'success': 'danger'}">${data ? 'працює': 'лежить'}</b>`);
+            }
+        })
+    }
+    function getResult(needRender) {
+        var $input = $('#txt-input');
+        var resources = String($input.val()).split('\n');
+        resources.forEach((myRes, i) => {
+            var res = String(myRes).trim();
+            if (!res) { return; }
+            if (!res.startsWith('http')) { res = 'https://' + res; }
+            if (needRender) {
+                $tableData.append(`
+                    <tr data-res="${res}">
+                        <td>${i + 1}</td>
+                        <td><a href="${res}" target="_blank" rel="noindex nofollow">${res}</a></td>
+                        <td data-href="${res}"><b class="badge badge-secondary">запит в процессі</b></td>
+                        <td><a href="https://check-host.net/check-ping?host=${encodeURIComponent(res)}" target="_blank" rel="noindex nofollow">Подробиці</a></td>
+                    </tr>
+                `);
+            } else {
+                $(`[data-href="${res}"]`)
+                    .empty()
+                    .append(`<b class="badge badge-secondary">запит в процессі</b>`);
+            }
+            requestRes(res);
+        });
+    }
+    $('.js-check').on('click', () => {
+        getResult(true);
+        const $interval = $('.js-interval');
+        if ($interval.val() && $interval.val() > 0) {
+            setInterval(getResult, $interval.val() * 1000 * 60);
+        }
+    });
+</script>
+</body>
+</html>
